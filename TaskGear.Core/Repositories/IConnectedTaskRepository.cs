d201c8d233b1267619d7cfe55be0679e332ca5a0
Emin <eminus.prime@gmail.com>
2025-04-08 21:39:12 +0200

    Add Core repositories

---
+++ TaskGear.Core/Repositories/IConnectedTaskRepository.cs
@@ -0,0 +1,8 @@
+using TaskGear.Core.Models;
+
+namespace TaskGear.Core.Repositories;
+
+public interface IConnectedTaskRepository: IGenericRepository<ConnectedTask> 
+{
+    
+}
---
+++ TaskGear.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,9 @@
+using System.Linq.Expressions;
+using TaskGear.Core.Models;
+
+namespace TaskGear.Core.Repositories;
+
+public interface IUserRepository : IGenericRepository<User> 
+{
+    
+}
