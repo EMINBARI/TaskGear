4808231753512eb93887eb24dc768cf305da8348
Emin <eminus.prime@gmail.com>
2025-04-12 21:42:15 +0200

    Add references to Core, Infrastructure and Application

--- TaskGear.Console/Program.cs
+++ TaskGear.Console/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Security.Authentication.ExtendedProtection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TaskGear.Console;
+
+class Program
+{
+    static async Task Main ()
+    {
+        IServiceCollection services = new ServiceNameCollection;
+    }
+
+}
