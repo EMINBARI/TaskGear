f24868a9acd7cfaebaaffbc72a0454df4ac03107
Emin <eminus.prime@gmail.com>
2025-05-01 22:01:49 +0200

    Add users CRUD operations

--- TaskGear.Api/Controllers/UsersController.cs
+++ TaskGear.Api/Controllers/UsersController.cs
@@ -19,8 +19,39 @@ public class UsersController : ControllerBase
     public async Task<ActionResult<List<UserResponse>>> GetUsers()
     {
         var users = await _userService.GetUsersAsync();
-        Console.WriteLine(users);
+        return Ok(users);
+    }
 
-        return Ok(users );
-    } 
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<UserResponse>> GetUser(Guid id)
+    {
+        var user = await _userService.GetUserAsync(id);
+        return Ok(user);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<UserResponse>> CreateUser([FromBody] AddUserRequest request)
+    {
+        var user = await _userService.AddUserAsync(
+            request
+        );
+
+        return Ok(user);
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<ActionResult<UserResponse>> UpdateUser(Guid id, [FromBody] UpdateUserRequest request)
+    {
+        var user = await _userService.EditUserAsync(request);
+        return Ok(user);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public async Task<ActionResult> DeleteUser(Guid id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return Ok(
+            new { message = $"User  with id {id} successfully deleted" }
+        );
+    }
 }
