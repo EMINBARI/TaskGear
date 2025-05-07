46ddb382f0ab2af5a81d15ed5cf3770b92a0645c
Emin <eminus.prime@gmail.com>
2025-05-07 19:22:07 +0200

    Add project member controller

---
+++ TaskGear.Api/Controllers/ProjectMembersController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskGear.Application.Services;
+using TaskGear.Application.Services.Contracts;
+
+namespace TaskGear.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProjectMembersController : ControllerBase
+{
+    private readonly IProjectMemberService _projectMemberService;
+
+    public ProjectMembersController(IProjectMemberService projectMemberService) {
+        _projectMemberService = projectMemberService;
+    }
+
+
+    [HttpGet("{id: guid}")]
+    public async Task<ActionResult<ProjectMemberResponse>> GetProjectMember(Guid Id)
+    {
+        var projectMember = await _projectMemberService.GetProjectMemberAsync(Id);
+        return Ok(projectMember);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<ProjectMemberResponse>> GetProjectMembers()
+    {
+        var projectMembers = await _projectMemberService.GetProjectMembersAsync();
+        return Ok(projectMembers);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ProjectMemberResponse>> CreateProjectMember([FromBody] AddProjectMemberRequest request)
+    {
+        var projectMember = await _projectMemberService.AddProjectMemberAsync(request);
+
+        return Ok(projectMember);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult<ProjectMemberResponse>> UpdateProjectMember([FromBody] UpdateProjectMemberRequest request)
+    {
+        var projectMember = await _projectMemberService.EditProjectMemberAsync(request);
+        return Ok(projectMember);
+    }
+
+    [HttpDelete("{id: guid}")]
+    public async Task<ActionResult> DeleteProjectMember(Guid id)
+    {
+        await _projectMemberService.DeleteProjectMemberAsync(id);
+        return Ok(
+            new { message = $"Project member with id {id} successfully deleted" }
+        );
+    }
+}
