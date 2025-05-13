019fd2206f8227ffc371f6a91318404202c1a653
Emin <eminus.prime@gmail.com>
2025-05-13 20:22:20 +0200

    Fix Project Member response model

--- TaskGear.Application/Contracts/ProjectMember/Responses/ProjectMemberResponse.cs
+++ TaskGear.Application/Contracts/ProjectMember/Responses/ProjectMemberResponse.cs
@@ -5,14 +5,14 @@ namespace TaskGear.Application.Services.Contracts;
 public class ProjectMemberResponse 
 {
     public Guid Id {get; set;}
-    public Project Project {get; set;}
-    public User User {get; set;}
+    public ProjectResponse Project {get; set;}
+    public UserResponse User {get; set;}
 
     public ProjectMemberResponse(ProjectMember projectMember)
     {
         Id = projectMember.Id;
-        Project = projectMember.Project;
-        User = projectMember.User;
+        Project = new ProjectResponse(projectMember.Project);
+        User = new UserResponse(projectMember.User);
     }
 
 }
