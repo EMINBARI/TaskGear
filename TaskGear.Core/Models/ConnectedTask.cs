e96b5153003109ad37869a7df96e743090f82c22
Emin <eminus.prime@gmail.com>
2025-05-16 21:03:06 +0200

    Update Connected Task core model

--- TaskGear.Core/Models/ConnectedTask.cs
+++ TaskGear.Core/Models/ConnectedTask.cs
@@ -4,11 +4,30 @@ namespace TaskGear.Core.Models;
 
 public class ConnectedTask: IEntity
 {
-    public Guid Id { get; set; }
-    public Guid TaskId { get; set; }
-    public ProjectTask Task { get; set; }
+    public Guid Id {get; set;}
+    public Guid TaskId1 { get; set; }
+    public Guid TaskId2{ get; set; }
 
+    public ProjectTask Task1 { get; set; }
+    public ProjectTask Task2{ get; set; }
+    
     public ConnectedTask() {}
+
+    public ConnectedTask
+    (
+        Guid id, 
+        Guid taskId1, 
+        ProjectTask task1,
+        Guid taskId2,
+        ProjectTask task2
+    )
+    {  
+        Id = id;
+        TaskId1 = taskId1;
+        Task1 = task1;
+        TaskId2 = taskId2;
+        Task2 = task2;
+    }
 }
 
 
