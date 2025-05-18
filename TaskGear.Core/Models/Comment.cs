f781960f948e827e27315b5e1b55e3a6b437066b
Emin <eminus.prime@gmail.com>
2025-05-18 22:21:31 +0200

    Update Comment Entity

--- TaskGear.Core/Models/Comment.cs
+++ TaskGear.Core/Models/Comment.cs
@@ -12,13 +12,23 @@ public class Comment: IEntity
     public ProjectTask Task { get; set; }
 
     public string Content { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
 
     public Comment () {}
 
-    // public Comment (Guid id, Guid createdBy, User createdByUser, Guid taskId, Task task) {
-
-    // }
-
-
+    public Comment(
+        Guid id, 
+        Guid createdBy, 
+        ProjectMember createdByUser, 
+        Guid taskId, 
+        ProjectTask task, 
+        string content)
+    {
+        Id = id;
+        CreatedBy = createdBy;
+        CreatedByMember = createdByUser;
+        TaskId = taskId;
+        Task = task;
+        Content = content;
+    }
 }
